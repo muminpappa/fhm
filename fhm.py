ab453aa9b3b361f016740050efc88eda6080de1d
muminpappa <pappa@mumintroll.org>
2020-11-14 21:51:19 +0100

    Fixed lint

--- fhm.py
+++ fhm.py
@@ -4,15 +4,17 @@ import requests
 import wget
 import datetime
 
-resp = requests.get('https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/statistik-och-analyser/bekraftade-fall-i-sverige/')
+resp = requests.get('https://www.folkhalsomyndigheten.se/'+
+    'smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/'+
+    'statistik-och-analyser/bekraftade-fall-i-sverige/')
 if resp.ok:
     soup = BeautifulSoup(resp.text, 'html.parser')
-    result = soup.find_all('a', {'title':'Excel-fil'})
+    result = soup.find_all('a', {'title': 'Excel-fil'})
 else:
-    print ("Boo! {}".format(resp.status_code))
-    print (resp.text)
+    print("Boo! {}".format(resp.status_code))
+    print(resp.text)
 
 url = result[0].get('href')
 outfile = 'fhm_download_'+str(datetime.date.today())+'.xlsx'
 
-wget.download(url, out = outfile)
+wget.download(url, out=outfile)
